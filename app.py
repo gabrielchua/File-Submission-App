dba02347b512e53700c7ff58dc16c98a5929be03
gabrielchua <34762009+gabrielchua@users.noreply.github.com>
2022-08-20 17:29:54 +0800

    Update app.py
    loading spinner, balloons, info box

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 
 
 st.set_page_config(page_title="Test")
 
-st.title("Please submit your report here.")
+st.info('Hello - this is a prototype')
 
+st.title("Please submit your report here.")
 
 
 uploaded_file = st.file_uploader("Upload the workload report using the provided template.")
 
 if uploaded_file is not None:
-  df = pd.read_excel(uploaded_file)
-
-  columns_received = list(df.columns)
-  columns_expected = ["ID", "Date_Of_Visit", "Claim"]
-
-  duplicate_rows = df.duplicated(keep = False)
+	
+	with st.spinner('Checking File'):
+		df = pd.read_excel(uploaded_file)
+		columns_received = list(df.columns)
+		columns_expected = ["ID", "Date_Of_Visit", "Claim"]
+		duplicate_rows = df.duplicated(keep = False)
+		time.sleep(1)
   
-  if not np.array_equal(columns_received,columns_expected , equal_nan=False):
-  	st.error('Do not modify the headers of the template.  \n Received: {}   \n Expected: {}'.format(columns_received, columns_expected), icon = "🚨")
+	if not np.array_equal(columns_received,columns_expected , equal_nan=False):
+		st.error('Do not modify the headers of the template.  \n Received: {}   \n Expected: {}'.format(columns_received, columns_expected), icon = "🚨")
 
-  elif np.sum(duplicate_rows):
-  	st.error('Duplicate entries detected.  \n Please check rows {}'.format(list(df.index[duplicate_rows].values)), icon="🚨")
+	elif np.sum(duplicate_rows):
+		st.error('Duplicate entries detected.  \n Please check rows {}'.format(list(df.index[duplicate_rows].values)), icon="🚨")
 
-  else: 
-  	st.success("Thank you for submitting the report!", icon = "✅")
+	else: 
+		st.success("Thank you for submitting the report!", icon = "✅")
+		st.balloons()
