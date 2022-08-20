1621bc73ae8949ba4dd0ed594da5ad5895ee958b
gabrielchua <34762009+gabrielchua@users.noreply.github.com>
2022-08-20 15:00:52 +0800

    Added v0.1 of app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.set_page_config(page_title="Test")
+
+st.title("Please submit your report here.")
+
+
+
+uploaded_file = st.file_uploader("Upload the workload report using the provided template.")
+
+if uploaded_file is not None:
+  df = pd.read_excel(uploaded_file)
+
+  columns_received = list(df.columns)
+  columns_expected = ["ID", "Date_Of_Visit", "Claim"]
+
+  duplicate_rows = df.duplicated(keep = False)
+  
+  if not np.array_equal(columns_received,columns_expected , equal_nan=False):
+  	st.error('Do not modify the headers of the template.  \n Received: {}   \n Expected: {}'.format(columns_received, columns_expected), icon = "🚨")
+
+  elif np.sum(duplicate_rows):
+  	st.error('Duplicate entries detected.  \n Please check rows {}'.format(list(df.index[duplicate_rows].values)), icon="🚨")
+
+  else: 
+  	st.success("Thank you for submitting the report!", icon = "✅")
